7989f2a50d07eb9f003a10f4986a43284f47df9a
soumyajeetsengupta <senguptasoumyajeet@outlook.com>
2024-01-23 13:44:19 +0530

    Day 17 Leetcode

--- Day 17/Day_17_Chunk_Array.js
+++ Day 17/Day_17_Chunk_Array.js
@@ -1,4 +1,7 @@
 /*
+__________________________________________________________________________________________________________________________________________________________________________________________________________
+
+
 Given an array arr and a chunk size size, return a chunked array. A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
 
 You may assume the array is the output of JSON.parse. In other words, it is valid JSON.
@@ -39,7 +42,7 @@ Constraints:
     2 <= JSON.stringify(arr).length <= 105
     1 <= size <= arr.length + 1
 
-
+__________________________________________________________________________________________________________________________________________________________________________________________________________
 */
 
 var chunk = function(arr, size) {
